e4d0738299b85245c00a4cddc76b5c391602e4a0
Ravi teja <raviteja@echortech.com>
2024-09-06 12:34:22 +0530

    png added and animation don properly

--- Pages/Home/SinglePage.jsx
+++ Pages/Home/SinglePage.jsx
@@ -1,23 +1,30 @@
 import React from "react"
-import { Text, View, Image, StyleSheet, } from "react-native"
-import Animated, { FadeIn, FadeInDown } from 'react-native-reanimated';
+import { Text, View, Image, StyleSheet, Pressable } from "react-native"
+import Animated, { FadeInDown } from 'react-native-reanimated';
 
 
 const SinglePage = ({ navigation, route }) => {
     const { item } = route.params
+    const AinmatedPressable = Animated.createAnimatedComponent(Pressable)
     return (
         <View style={Styles.container} >
             <Animated.Image
                 sharedTransitionTag={item.name}
-                source={{ uri: item.dogPic }}
-                style={{ height: 200 }}
+                source={item.dogPic}
+                resizeMode={'cover'}
+                style={{ height: 300,width:"100%" }}
             />
             <Animated.View
                 // style={styles.textContainer}
-                entering={FadeIn.delay(600)}>
+                entering={FadeInDown.duration(1000)}>
                 <Text onPress={navigation.goBack} style={{ paddingBottom: 30 }} >{item.name}</Text>
                 <Text >{item.decs}</Text>
             </Animated.View>
+
+            <AinmatedPressable style={Styles.buttonContainer} entering={FadeInDown.duration(900)} onPress={() => console.log('Booking Ticket')} >
+                <Text style={{ color: "white", fontWeight: "600" }}>Buy Now</Text>
+            </AinmatedPressable>
+
         </View >
     )
 }
@@ -26,7 +33,15 @@ const SinglePage = ({ navigation, route }) => {
 const Styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff"
+        backgroundColor: "#fff",
+    },
+    buttonContainer: {
+        width: "90%",
+        alignItems: "center",
+        padding: 30,
+        backgroundColor: "blue",
+        alignSelf: "center",
+        borderRadius: 10,
     }
 })
 
